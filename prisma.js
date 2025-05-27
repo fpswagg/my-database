1120bcb420fe83786b27f294039281c408e407a3
fpswagg <essoungo@gmail.com>
2025-05-27 17:55:19 +0100

    Better prisma+

--- prisma.js
+++ prisma.js
@@ -1,10 +1,9 @@
 "use strict";
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-// src/prisma.ts
-const client_1 = require("@prisma/client");
-exports.prisma = (_a = global.prisma) !== null && _a !== void 0 ? _a : new client_1.PrismaClient();
-if (process.env.NODE_ENV !== 'production')
+const src_1 = require("./src");
+exports.prisma = global.prisma || new src_1.PrismaClient();
+if (process.env.NODE_ENV !== 'production') {
     global.prisma = exports.prisma;
+}
 exports.default = exports.prisma;
--- prisma.ts
+++ prisma.ts
@@ -1,12 +1,13 @@
-// src/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './src';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ?? new PrismaClient();
+export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+    global.prisma = prisma;
+}
 
 export default prisma;
