569a111eac70816584b34bec2ab7bc384c6a2886
fpswagg <essoungo@gmail.com>
2025-05-29 11:49:18 +0100

    Returned to old prisma

--- prisma.js
+++ prisma.js
@@ -2,8 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const src_1 = require("./src");
-exports.prisma = global.prisma || new src_1.PrismaClient();
-if (process.env.NODE_ENV !== 'production') {
-    global.prisma = exports.prisma;
-}
+exports.prisma = new src_1.PrismaClient();
 exports.default = exports.prisma;
