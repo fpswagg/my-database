8cdedb99c8f6d692bbc13f7269c4bbe1c9ab928d
fpswagg <essoungo@gmail.com>
2025-05-29 11:47:19 +0100

    Returned to old prisma

--- prisma.ts
+++ prisma.ts
@@ -1,13 +1,5 @@
 import { PrismaClient } from './src';
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-
-export const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma;
-}
+export const prisma = new PrismaClient();
 
 export default prisma;
